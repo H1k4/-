c92e4fd952c421f102b01c3d8587d5442160f01f
H1k4 <145106093+H1k4@users.noreply.github.com>
2024-03-29 18:01:22 +1000

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,26 @@
+import {NAME_MASS, AGE_MASS, JOB_MASS} from "../massive/module.js";
+let personItemMass = document.querySelectorAll(".person-item");
 let title = document.querySelector(".main-title");
 let person = document.querySelector(".person");
 let button = document.getElementById("create");
+function random(min,max) {
+    return Math.floor(Math.random() * (max - min) + min);   
+}
 
 console.log(title,person,button);
- console.dir(title);
+console.dir(title);
 button.addEventListener("click",()=>{
     console.log("cdv")
     title.style.opacity = 0;
+    console.log(random);
+    personItemMass[0].innerText ="Имя:" + NAME_MASS[random(0,NAME_MASS.length)];
+    personItemMass[1].innerText ="Возраст:" + AGE_MASS[random(0,AGE_MASS.length)];
+    personItemMass[2].innerText ="Профессия:" + JOB_MASS[random(0,JOB_MASS.length)];
     setTimeout(()=> {
         title.style.display = 'none';
-        person.style.opacity = 100
         person.style.display =  'flex'; 
- }, 1000 );
-        
-    
+        setTimeout( ()=> {
+            person.style.opacity = 100
+    }, 1000)  
+ }, 1000);   
 })
---
+++ assets/massive/module.js
@@ -0,0 +1,4 @@
+const NAME_MASS = ["Биба", "Боба","Ангон","Полnна","Romochka"];
+const AGE_MASS = ["7","88","666","30",];
+const JOB_MASS = ["Программист","Проститутка","Уборщик","Строитель","Кассир","Инженер"];
+export{NAME_MASS, AGE_MASS, JOB_MASS};
